d1388ea7a4be25912dbe9bf8e5cea2c2136c7e0b
Dante <andreev_9000@mail.ru>
2020-03-31 02:23:22 +0700

    final

--- venv/main.py
+++ venv/main.py
@@ -1,5 +1,24 @@
+from tabulate import tabulate
+
+word = input('Введите сообщение:\n').upper()
+_list = [int(x) for x in input('Введите шифр:\n').split(',')]
+_dict = {key:'' for key in _list}
+
+count = 0
+while count < len(word):
+    for key in _dict:
+        if count < len(word):
+            _dict[key] += word[count]
+            count+=1
+        else:
+            _dict[key] += '-'
 
 
-word = input('Введите сообщение').upper()
-_list = [int(x) for x in input('Введите шифр').split(', ')]
+print(tabulate(_dict, headers='keys', tablefmt='grid'))
 
+sort = sorted(_dict)
+text = ''
+for key in sort:
+    text += _dict[key]
+
+print(text)
